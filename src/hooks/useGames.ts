bf08e6782eb651ffda7e03b7d42ff12b1e4fbf48
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2023-10-18 06:56:58 +0530

    Implemented Searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,6 +23,7 @@ const useGames = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        searchText: gameQuery.searchText,
       },
     },
     [gameQuery]
