161e6f8f9476b7ef698a4cadb59e58b0393db65f
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2023-10-18 17:46:09 +0530

    Shipped Platforms with the app

--- src/hooks/usePlatform.tsx
+++ src/hooks/usePlatform.tsx
@@ -1,11 +1,11 @@
-import useData from "./useData";
+import platforms from "../data/platforms";
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+// interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
-const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+const usePlatform = () => ({ data: platforms, error: null });
 
 export default usePlatform;
